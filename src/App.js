608ca5b5376bf74290a040a8118eff9a4aac2611
KimuraTetsuro <haribote@Harrys-MBA-Dash.local>
2016-10-27 16:56:19 +0900

    add login button.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import icon_433_github from './icons/icon_433_github.svg';
 import './App.css';
 
 class App extends Component {
+  /**
+   * @returns {XML}
+   */
   render() {
+    // JSX template
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React Firebase Chat</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-login">
+          <button type="button"><img src={icon_433_github} alt="" width={64} height={64} />Login with GitHub.</button>
+        </div>
       </div>
     );
   }
