ea957d4dac31e3ce37fca3d7189d82f1f43dff7a
KimuraTetsuro <haribote@Harrys-MBA-Dash.local>
2016-10-27 15:37:41 +0900

    install firebase module and init config.

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+/**
+ * Firebaseのコンフィグ
+ * @type {{apiKey: string, authDomain: string, databaseURL: string, storageBucket: string, messagingSenderId: string}}
+ */
+export const firebaseConfig = {
+  apiKey           : '',
+  authDomain       : '',
+  databaseURL      : '',
+  storageBucket    : '',
+  messagingSenderId: ''
+};
